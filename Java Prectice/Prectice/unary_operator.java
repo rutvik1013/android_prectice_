c34e056308ae46eec3fd9c5d3f76e3329f9ce9ea
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2024-12-21 12:06:18 +0530

    Add files via upload

---
+++ Java Prectice/Prectice/unary_operator.java
@@ -0,0 +1,16 @@
+public class unary_operator {
+    public static void main(String[] args) {
+        
+        // Interger declared
+        int a = 10;
+        int b = 10;
+
+        // Using unary operators
+        System.out.println("Postincrement : " + (a++));
+        System.out.println("Preincrement : " + (++a));
+
+        System.out.println("Postdecrement : " + (b--));
+        System.out.println("Predecrement : " + (--b));
+    }
+    
+}
