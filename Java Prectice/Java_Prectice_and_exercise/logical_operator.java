f034bc1957779336110b7c37e2410497c254c66c
rutvik1013 <113574475+rutvik1013@users.noreply.github.com>
2025-01-03 10:02:18 +0530

    Update logical_operator.java

--- Java Prectice/Java_Prectice_and_exercise/logical_operator.java
+++ Java Prectice/Java_Prectice_and_exercise/logical_operator.java
@@ -3,11 +3,23 @@
 public class logical_operator {
     public static void main(String[] args) {
          // Logical operators
-         boolean x = true;
-         boolean y = false;
-       
-         System.out.println("x && y: " + (x && y));
-         System.out.println("x || y: " + (x || y));
-         System.out.println("!x: " + (!x));
+        int a=6;
+        int b=8;
+        int c=9;
+        int d=7;
+
+        // Logical AND
+
+        // boolean result=a>b&&c>d;
+        
+        // Logical OR
+
+        // boolean result=a>b||c>d;
+
+        // Logical NOT
+
+        boolean result=a<b;
+
+        System.out.println(!result);
     } 
 }
